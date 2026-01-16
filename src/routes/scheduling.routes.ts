fcea8234465e27745a131e67b1145a6cfefe0cd3
wanderson.oliveira <ws49066@gmail.com>
2026-01-15 22:48:27 -0400

    criada rotas para o agentamento get, post and put

--- src/routes/scheduling.routes.ts
+++ src/routes/scheduling.routes.ts
@@ -1,3 +1,4 @@
+import { SchedulingController } from '@/controllers/SchedulingController'
 import { validateToken } from '@/middlewares/validateAuth'
 import { validadeUserClient } from '@/middlewares/validateClient'
 import { Router } from 'express'
@@ -5,11 +6,11 @@ import { Router } from 'express'
 
 const schedulingRoutes = Router()
 
-schedulingRoutes.post("/", validateToken, validadeUserClient) // Just user can do this
+schedulingRoutes.post("/", validateToken, validadeUserClient, SchedulingController.createScheduling) // Just user can do this
 
-schedulingRoutes.get("/", validateToken) // both
+schedulingRoutes.get("/", validateToken, SchedulingController.listScheduling) // both
 
-schedulingRoutes.put("/", validateToken) // both admin change status={Aproved // Cancel} / client just Cancel your scheduling
+schedulingRoutes.put("/", validateToken, SchedulingController.updateScheduling) // both admin change status={Aproved // Cancel} / client just Cancel your scheduling
 
 
 export default schedulingRoutes
